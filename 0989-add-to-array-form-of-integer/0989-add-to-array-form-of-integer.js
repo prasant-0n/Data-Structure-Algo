b0db632a4db4916882e1da3ec46d512245957b96
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-01-02 20:45:32 +0530

    Time: 14 ms (49.75%), Space: 62 MB (36.04%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -4,18 +4,29 @@
  * @return {number[]}
  */
 var addToArrayForm = function (num, k) {
-    let res = [];
-    let i = num.length - 1;
-    let carry = k;
+    // let res = [];
+    // let i = num.length - 1;
+    // let carry = k;
 
-    while (i >= 0 || carry > 0) {
-        if (i >= 0) {
-            carry += num[i];
-            i--;
-        }
-        res.push(carry % 10);
-        carry = Math.floor(carry / 10);
-    }
+    // while (i >= 0 || carry > 0) {
+    //     if (i >= 0) {
+    //         carry += num[i];
+    //         i--;
+    //     }
+    //     res.push(carry % 10);
+    //     carry = Math.floor(carry / 10);
+    // }
 
-    return res.reverse();
+    // return res.reverse();
+
+
+    /**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+    let sum = BigInt(num.join('')) + BigInt(k);
+    let convertSum = sum.toString().split('').map((num) => parseInt(num));
+    return convertSum;
+
 };
