4ab22d50ea47e08f03aecc0be550e158ac6cf5bc
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-05-14 13:53:41 +0530

    Time: 1 ms (59.38%), Space: 61.2 MB (62.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,11 +3,21 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    // i starts at the end, j starts at the beginning
-    for (let i = s.length - 1, j = 0; j < i; i--, j++) {
-        // Swap elements
-        let temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
+    // // i starts at the end, j starts at the beginning
+    // for (let i = s.length - 1, j = 0; j < i; i--, j++) {
+    //     // Swap elements
+    //     let temp = s[i];
+    //     s[i] = s[j];
+    //     s[j] = temp;
+    // }
+
+    let left = 0;
+    let right = s.length -1;
+
+    while( left < right){
+        [s[left], s[right]] = [s[right],s[left]];
+
+        left++;
+        right --;
     }
 };
